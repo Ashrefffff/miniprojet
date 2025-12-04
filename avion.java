708a180405265531f2a656666c49217b17746f0e
Ashrefffff <ashrefsaid11@gmail.com>
2025-12-04 22:52:52 +0100

    Create Avion class with flight management methods

---
+++ avion.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Avion {
+    private String immatriculation;
+    private String modele;
+    private int capacite;
+    private ArrayList<Vol> vols;
+    public Avion(String immatriculation, String modele, int capacite) {
+        this.immatriculation = immatriculation;
+        this.modele = modele;
+        this.capacite = capacite;
+        this.vols = new ArrayList<>();
+    }
+    public void affecterVol(Vol vol) {
+        vols.add(vol);
+        System.out.println("Vol " + vol.getNumeroVol() + " affecté à l'avion " + immatriculation);
+    }
+    public boolean verifierDisponibilite(Vol vol) {
+        for (Vol v : vols) {
+            if (v.getDateHeureDepart().equals(vol.getDateHeureDepart())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
