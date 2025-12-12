88f6bd2571588214385ff13eaa21732ce23e5f9c
Ali-sannan <ali.sannan@eleve.isep.fr>
2025-12-12 18:32:48 +0100

    Update personnelcabine.java

--- personnelcabine.java
+++ personnelcabine.java
@@ -1,1 +1,22 @@
+package com.example.API.model;
+
+import com.example.API.model.Vol;
+
+public class PersonnelCabine extends Employe {
+    private String qualification;
+    public PersonnelCabine(String identifiant, String nom, String adresse, String contact, String numeroEmploye, String dateEmbauche, String qualification) {
+        super(identifiant, nom, adresse, contact, numeroEmploye, dateEmbauche);
+        this.qualification = qualification;
+    }
+    public void affecterVol(Vol vol) {
+        System.out.println("Personnel cabine affecté au vol: " + vol.getNumeroVol());
+    }
+    public void obtenirVol(Vol vol) {
+        System.out.println("Détails du vol: " + vol.getNumeroVol());
+    }
+    @Override
+    public String obtenirRole() {
+        return "Rôle: Personnel Cabine";
+    }
+}
 
