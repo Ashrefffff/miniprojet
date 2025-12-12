e6daa21514d0ef1b29571331e2b7d3109431463a
Ali-sannan <ali.sannan@eleve.isep.fr>
2025-12-12 18:28:17 +0100

    Update aeroport.java

--- aeroport.java
+++ aeroport.java
@@ -1,1 +1,22 @@
+package com.example.API.model;
+import com.example.API.model.Vol;
+
+import java.util.ArrayList;
+
+public class Aeroport {
+    private String nom;
+    private String ville;
+    private String description;
+    private ArrayList<Vol> vols;
+    public Aeroport(String nom, String ville, String description, String s) {
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+        this.vols = new ArrayList<>();
+    }
+    public void affecterVol(Vol vol) {
+        vols.add(vol);
+        System.out.println("Vol " + vol.getNumeroVol() + " affecté à l'aéroport " + nom);
+    }
+}
 
