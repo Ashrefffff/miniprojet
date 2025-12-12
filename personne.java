f4910dabdaba9bd27d377eb05d3e45c308f807cc
Ali-sannan <ali.sannan@eleve.isep.fr>
2025-12-12 18:32:26 +0100

    Update personne.java

--- personne.java
+++ personne.java
@@ -1,1 +1,24 @@
+package com.example.API.model;
+
+public class Personne {
+    private String identifiant;
+    private String nom;
+    private String adresse;
+    private String contact;
+    public Personne(String identifiant, String nom, String adresse, String contact) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+    public void obtenirInfos() {
+        System.out.println("Identifiant: " + identifiant);
+        System.out.println("Nom: " + nom);
+        System.out.println("Adresse: " + adresse);
+        System.out.println("Contact: " + contact);
+    }
+    public String obtenirRole() {
+        return "Role non défini pour la personne générale.";
+    }
+}
 
