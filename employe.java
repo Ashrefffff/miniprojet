2ae0de4247f60862180871eabcb125dbdde6ed19
Ali-sannan <ali.sannan@eleve.isep.fr>
2025-12-12 18:31:08 +0100

    Update employe.java

--- employe.java
+++ employe.java
@@ -1,1 +1,15 @@
+package com.example.API.model;
+
+public class Employe extends Personne {
+    private String numeroEmploye;
+    private String dateEmbauche;
+    public Employe(String identifiant, String nom, String adresse, String contact, String numeroEmploye, String dateEmbauche) {
+        super(identifiant, nom, adresse, contact);
+        this.numeroEmploye = numeroEmploye;
+        this.dateEmbauche = dateEmbauche;
+    }
+    public String obtenirRole() {
+        return "Role de l'employé: Employé générique";
+    }
+}
 
