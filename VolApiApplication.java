136c9355e5e9056181e4ccdec419ec0a92a3c075
Ali-sannan <ali.sannan@eleve.isep.fr>
2025-12-12 18:43:03 +0100

    Create VolApiApplication.java

---
+++ VolApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VolApiApplication.class, args);
+    }
+}
